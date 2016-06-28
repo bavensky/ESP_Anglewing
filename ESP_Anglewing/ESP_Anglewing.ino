308622866b2998452e8b754c8ce2c2b28867355c
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2016-06-28 17:25:05 +0700

    no message

--- ESP_Anglewing/ESP_Anglewing.ino
+++ ESP_Anglewing/ESP_Anglewing.ino
@@ -1,27 +1,44 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "Wire.h"
+#include <Servo.h> 
+
+Servo L_wing;
+Servo R_wing;
 
 MPU6050 accelgyro;
+#define OUTPUT_READABLE_ACCELGYRO
+
+
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
+float f_angle, pitchAcc, gyroXrate;
+float P_CompCoeff = 0.95;
+int timer = 10;
 
-#define OUTPUT_READABLE_ACCELGYRO
 void setup() {
   Wire.begin();
   Serial.begin(38400);
+  
   accelgyro.initialize();
-}
 
+  L_wing.attach(14);
+  R_wing.attach(12);
+}
+unsigned long time_prev;
 void loop() {
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  Serial.print("acc = ");
-  Serial.print(ax); Serial.print("\t");
-  Serial.print(ay); Serial.print("\t");
-  Serial.print(az); Serial.print("\t");
-  Serial.print("gyro = ");
-  Serial.print(gx); Serial.print("\t");
-  Serial.print(gy); Serial.print("\t");
-  Serial.println(gz);
+  unsigned long time_now = millis();
+  if (time_now - time_prev >= 10)
+  {
+    time_prev = time_now;
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    f_angle += (((float)gz / 16.4f) * (-0.01f));
+    float pitchAcc = atan2(ax, -ay) * RAD_TO_DEG; //(float)22/7; //RAD_TO_DEG
+    f_angle = P_CompCoeff * f_angle + (1.0f - P_CompCoeff) * pitchAcc;
+    //    Serial.print("  f_angle = ");
+    //    Serial.println(f_angle);
+  }
+  L_wing.write(0); 
+  R_wing.write(0); 
 }
